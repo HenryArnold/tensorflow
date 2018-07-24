dafb6e5ec722b7d21b00fb0d8526755e1db13206
NetWallow <example@gmail.com>
2018-07-24 12:47:07 +0800

    sesson test

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 import tensorflow as tf
 import numpy as np
-
+"""
 #create data
 x_data=np.random.rand(100).astype(np.float32)
 y_data=x_data*0.1 + 0.3
@@ -31,3 +31,20 @@ for step in range(201):
     sess.run(train)
     if step % 20 == 0:
         print(step, sess.run(Weight), sess.run(biases))
+"""
+matrix1 = tf.constant([[3,3]])
+matrix2 = tf.constant([[2],
+                       [2]])
+
+product = tf.matmul(matrix1, matrix2)
+
+# method 1
+sess = tf.Session()
+result=sess.run(product)
+print(result)
+sess.close()
+
+# method 2
+with tf.Session() as sess:
+    result2 = sess.run(product)
+    print(result2)
